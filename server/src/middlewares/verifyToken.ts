d0899b5885d14732fb7eaec7e804c0cfeeda5b67
denisbilyal <denisbilyal@gmail.com>
2025-05-20 15:30:07 +0300

    Move to server folder

---
+++ server/src/middlewares/verifyToken.ts
@@ -0,0 +1,26 @@
+import { verify } from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import { IAuthenticatedUserRequest, IUser } from "../interfaces/userInterface";
+
+
+export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+	const token = authHeader && authHeader.split(' ').pop();
+
+    if (!token) {
+        res.status(401).send({ error: "No token provided" });
+        return;
+    }
+
+    try {
+        const user = verify(token, process.env.ACCESS_TOKEN as string) as IUser;
+        (req as IAuthenticatedUserRequest).user = user;
+    }
+    catch (err) {
+        res.status(403).send({ error: "Invalid token" });
+        return;
+    }
+
+
+    next();
+}
